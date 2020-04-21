c1be275bc8efaf79f90965d6c69da329d283c12b
Kuk Roman <roman.kuk.pz.2017@lpnu.ua>
2020-04-21 20:30:28 +0300

    test ignore 20:30

--- diagram/diagram/Properties/Settings.Designer.cs
+++ diagram/diagram/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace diagram.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.2.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,8 +26,8 @@ namespace diagram.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-V5NE9IV\\MSSQLSERVER01;Initial Catalog=my_db_for_db;Integrated" +
-            " Security=True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-BCD77AK\\KUK_ROMAN_SERVER;Initial Catalog=my_db_for_db;Integra" +
+            "ted Security=True")]
         public string my_db_for_dbConnectionString {
             get {
                 return ((string)(this["my_db_for_dbConnectionString"]));
